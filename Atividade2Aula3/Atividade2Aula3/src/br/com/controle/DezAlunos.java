47be080309d5af1add6cd4824ceba56fb45317f4
Maria Eduarda R. Gabriel <142342951+MariaEduardaRibeiro0212@users.noreply.github.com>
2024-09-15 18:08:08 -0300

    Atividade finalizada
    Atividade finalizada.

---
+++ Atividade2Aula3/Atividade2Aula3/src/br/com/controle/DezAlunos.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.controle;
+
+/*Faça um programa que receba como dados de entrada duas notas de 10 alunos e mostre como resultado:
+a) Qual foi a maior média da turma;
+b) Qual foi a menor média da turma;
+c) Qual foi a média da turma;*/
+ 
+public class DezAlunos {
+    private double nota1;
+    private double nota2;
+    private double mediaA;
+    private double mediaT;
+    private double maiorMedia;
+    private double menorMedia = 10;
+
+    public double getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(double nota2) {
+        
+        this.nota2 = nota2;
+        this.mediaA = (this.nota1 + this.nota2)/2;
+        this.mediaT+=this.mediaA;
+        
+       if(this.mediaA < this.menorMedia){
+          this.menorMedia = this.mediaA; 
+       }
+       if(this.mediaA > this.maiorMedia ){
+            this.maiorMedia = this.mediaA;
+        }
+        
+    }
+
+    public double getMediaA(){
+        return mediaA;
+    }
+
+    public double getMediaT() {
+        return mediaT;
+    }
+
+    public double getMaiorMedia() {
+        return maiorMedia;
+    }
+
+    public double getMenorMedia() {
+        return menorMedia;
+    }
+      
+}
---
+++ Atividade2Aula3/Atividade2Aula3/src/br/com/visao/Objeto10Alunos.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.visao;
+
+import br.com.controle.DezAlunos;
+import javax.swing.JOptionPane;
+
+public class Objeto10Alunos {
+    public static void main(String[] args) {
+        DezAlunos d = new DezAlunos();
+        
+        int cont;
+        
+        for(cont = 1; cont <= 10; cont++){
+          
+          d.setNota1(Double.valueOf(JOptionPane.showInputDialog("Digite a nota do primeiro semestre: ")));
+          d.setNota2(Double.valueOf(JOptionPane.showInputDialog("Digite a nota do segundo semestre: ")));
+          
+        }
+        
+        JOptionPane.showMessageDialog(null, "A média da turma é " + d.getMediaT()/10);
+        JOptionPane.showMessageDialog(null, "A maior nota é " + d.getMaiorMedia());
+        JOptionPane.showMessageDialog(null, "A menor nota é " + d.getMenorMedia());
+        
+    }
+    
+}
