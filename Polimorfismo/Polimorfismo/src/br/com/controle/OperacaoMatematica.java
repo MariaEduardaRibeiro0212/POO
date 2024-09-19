2070b4c98914a268acd91f61daea61027858e742
Maria Eduarda R. Gabriel <142342951+MariaEduardaRibeiro0212@users.noreply.github.com>
2024-09-19 11:02:57 -0300

    Quatro operações
    Necessário fazer a validação dos campos.
    Terminar em casa.

---
+++ Polimorfismo/Polimorfismo/src/br/com/controle/OperacaoMatematica.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.controle;
+
+public class OperacaoMatematica {
+    public Double calcular(Double valor1, Double valor2){
+        return 0.0;
+    }
+}
