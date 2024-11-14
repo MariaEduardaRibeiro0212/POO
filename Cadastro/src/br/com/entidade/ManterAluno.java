4f6427ea0bb63c33dbb3156e01cca86674dcf301
Maria Eduarda R. Gabriel <142342951+MariaEduardaRibeiro0212@users.noreply.github.com>
2024-11-14 10:38:38 -0300

    crudJava
    Falta a função alterar.

---
+++ Cadastro/src/br/com/entidade/ManterAluno.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.entidade;
+import br.com.controle.Aluno;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author lorrany.magalhaes
+ */
+public class ManterAluno extends DAO{
+    public void inserir(Aluno a) throws Exception {
+        try {
+        abrirBanco();
+        String query = "INSERT INTO alunos(codigo,nome,email)"
+                + "values(null,?,?)";
+        pst=(PreparedStatement) con.prepareStatement(query);
+        pst.setString(1, a.getNome());
+        pst.setString(2, a.getEmail());
+        pst.execute();
+        fecharBanco();
+        }
+        catch (Exception e){
+            System.out.println("Erro " + e.getMessage());
+            
+        }
+    }
+    
+    //Listando alunos:
+            public ArrayList<Aluno> PesquisarTudo () throws Exception {
+       ArrayList<Aluno> alunos = new ArrayList<Aluno>();
+         try{
+         abrirBanco();  
+         String query = "select codigo, nome FROM alunos";
+         pst = (PreparedStatement) con.prepareStatement(query);
+         ResultSet tr = pst.executeQuery();
+         Aluno a ;
+         while (tr.next()){               
+           a = new Aluno();
+           a.setCodigo(tr.getInt("codigo"));
+           a.setNome(tr.getString("nome"));
+           alunos.add(a);
+         } 
+         fecharBanco();
+       }catch (Exception e){
+           System.out.println("Erro " + e.getMessage());
+     } 
+       return alunos;
+     }       
+    
+}
